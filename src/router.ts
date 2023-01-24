93f905dda5644c56e1c9ad6424c56ed61164dc44
Cesar Roman <cesardroman18@gmail.com>
2023-01-24 14:53:43 -0600

    added input validations on put and post routes

--- src/router.ts
+++ src/router.ts
@@ -1,17 +1,20 @@
 import { Router } from 'express';
+import { body, oneOf, validationResult } from 'express-validator';
+import handleInputErrors from './utils/middleware';
 
 const router = Router();
 
 /**
  * Product
  */
-router.get('/product', (req, res) => {
-  // res.json({ message: req.shh_secret });
-  res.json({ message: 'henloJson!' });
-});
+router.get('/product', (req, res) => {});
 router.get('/product/:id', () => {});
-router.put('/product/:id', () => {});
-router.post('/product', () => {});
+router.put(
+  '/product/:id',
+  [body('name').isString(), handleInputErrors],
+  (req, res) => {}
+);
+router.post('/product', [body('name').isString(), handleInputErrors], () => {});
 router.delete('/product/:id', () => {});
 
 /**
@@ -19,8 +22,22 @@ router.delete('/product/:id', () => {});
  */
 router.get('/update', () => {});
 router.get('/update/:id', () => {});
-router.put('/update/:id', () => {});
-router.post('/update', () => {});
+router.put(
+  '/update/:id',
+  [
+    body('title').optional().isString(),
+    body('body').optional().isString(),
+    body('status').isIn(['IN_PROGRESS', 'LIVE', 'DEPRECATED', 'ARCHIVED']),
+    body('version').optional().isString(),
+    body('version').optional().isString(),
+  ],
+  () => {}
+);
+router.post(
+  '/update',
+  [body('title').exists().isString(), body('body').exists().isString()],
+  () => {}
+);
 router.delete('/update/:id', () => {});
 
 /**
@@ -28,8 +45,23 @@ router.delete('/update/:id', () => {});
  */
 router.get('/updatepoint', () => {});
 router.get('/updatepoint/:id', () => {});
-router.put('/updatepoint/:id', () => {});
-router.post('/updatepoint', () => {});
+router.put(
+  '/updatepoint/:id',
+  [
+    body('name').optional().isString(),
+    body('description').optional().isString(),
+  ],
+  () => {}
+);
+router.post(
+  '/updatepoint',
+  [
+    body('name').isString(),
+    body('description').isString(),
+    body('updateId').exists().isString(),
+  ],
+  () => {}
+);
 router.delete('/updatepoint/:id', () => {});
 
 export default router;
