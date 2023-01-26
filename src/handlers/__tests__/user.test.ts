20599593fb53ed2419f6c6b20d0957ff3a9d0ab9
Cesar Roman <cesardroman18@gmail.com>
2023-01-26 16:55:46 -0600

    added createNewUser test

--- src/handlers/__tests__/user.test.ts
+++ src/handlers/__tests__/user.test.ts
@@ -1,5 +1,23 @@
+import { after } from 'node:test';
+import { createNewUser } from '../index';
+
 describe('user handler', () => {
-  it('should do something when something happens', () => {
-    expect(1).toBe(1);
+  //can use beforeEach, afterEach to make sure you delete everything after tests run. Also tests should be stateless and non-reliant on other tests
+
+  it('should create new user', async () => {
+    const req = {
+      body: {
+        username: 'hello',
+        password: 'hi',
+      },
+    };
+
+    const res = {
+      json({ token }) {
+        expect(token).toBeTruthy();
+      },
+    };
+
+    await createNewUser(req, res, () => {});
   });
 });
