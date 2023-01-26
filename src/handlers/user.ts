b875cada0c23a93686a5621cfdb5e51da695197a
Cesar Roman <cesardroman18@gmail.com>
2023-01-26 14:49:28 -0600

    added some error handlers

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -1,36 +1,45 @@
 import { prisma } from '../utils';
 import { createJWT, hashPassword, comparePasswords } from '../utils';
 
-//whern talking to a disc functions are alway async
-export const createNewUser = async (req, res) => {
-  const hashedPassword = await hashPassword(req.body.password);
+//when talking to a disc, functions are alway async
+export const createNewUser = async (req, res, next) => {
+  try {
+    const hashedPassword = await hashPassword(req.body.password);
 
-  const user = await prisma.user.create({
-    data: {
-      username: req.body.username,
-      password: hashedPassword,
-    },
-  });
+    const user = await prisma.user.create({
+      data: {
+        username: req.body.username,
+        password: hashedPassword,
+      },
+    });
 
-  const token = createJWT(user);
-  res.json({ token });
+    const token = createJWT(user);
+    res.json({ token });
+  } catch (err) {
+    err.type = 'input';
+    next(err);
+  }
 };
 
-export const signin = async (req, res) => {
-  const user = await prisma.user.findUnique({
-    where: {
-      username: req.body.username,
-    },
-  });
+export const signin = async (req, res, next) => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        username: req.body.username,
+      },
+    });
 
-  const isValid = await comparePasswords(req.body.password, user.password);
+    const isValid = await comparePasswords(req.body.password, user.password);
 
-  if (!isValid) {
-    res.status(401);
-    res.json({ message: 'nope' });
-    return;
+    if (!isValid) {
+      res.status(401);
+      res.json({ message: 'nope' });
+      return;
+    }
+
+    const token = createJWT(user);
+    res.json({ token });
+  } catch (err) {
+    next(err);
   }
-
-  const token = createJWT(user);
-  res.json({ token });
 };
